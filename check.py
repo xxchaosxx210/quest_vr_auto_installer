306fd6c2c9502c7cfc434e828e0869d490704c78
xxchaosxx210 <pmillar1279@googlemail.com>
2023-02-07 07:05:56 +0000

    comment added

--- check.py
+++ check.py
@@ -1,12 +1,13 @@
-# runs pyflakes. for code evaluation
+"""runs pyflakes. for code evaluation
+"""
 
 import os
 
-exclude_dirs = ['.venv', '.vscode', 'images', '__pycache__']
+exclude_dirs = [".venv", ".vscode", "images", "__pycache__"]
 
 for root, dirs, files in os.walk(os.getcwd()):
     dirs[:] = [d for d in dirs if d not in exclude_dirs]
     for file in files:
-        if file.endswith('.py'):
+        if file.endswith(".py"):
             py_path = os.path.join(root, file)
-            os.system(f'pyflakes {py_path}')
+            os.system(f"pyflakes {py_path}")
