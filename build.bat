50b7a75da0c6cfef3ec9ef8deab0fac40ef881d2
xxchaosxx210 <pmillar1279@googlemail.com>
2023-02-11 18:27:32 +0000

    inno setup is working
    still need a good test

--- build.bat
+++ build.bat
@@ -7,6 +7,6 @@ pyinstaller --clean ^
 --exclude=pyinstaller --exclude=pyflakes --exclude=autopep8 --exclude=black --exclude pyinstaller-hooks-contrib --exclude pylint --exclude pytest --exclude pytest-asyncio ^
 --add-data ".\adblib\win64;adblib\win64" ^
 --add-data "images\*;images" ^
---add-data "deluge-setup\*;deluge-setup" ^
+--add-data "deluge\bin\version-211\win64\;deluge\bin\version-211\win64" ^
 --icon .\images\icon.ico ^
 app.py
