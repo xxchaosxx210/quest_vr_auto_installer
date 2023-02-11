ebeb835104bdf818d91316f509818777b1a39eb2
xxchaosxx210 <pmillar1279@googlemail.com>
2023-02-11 17:36:18 +0000

    added icon to build

--- build.bat
+++ build.bat
@@ -6,5 +6,7 @@ pyinstaller --clean ^
 -w ^
 --exclude=pyinstaller --exclude=pyflakes --exclude=autopep8 --exclude=black --exclude pyinstaller-hooks-contrib --exclude pylint --exclude pytest --exclude pytest-asyncio ^
 --add-data ".\adblib\win64;adblib\win64" ^
+--add-data "images\*;images" ^
+--add-data "deluge-setup\*;deluge-setup"
 --icon .\images\icon.ico ^
 app.py
